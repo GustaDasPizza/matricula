94d5b7895e7e4865e4fd1ba4cf5b4824c4266bc1
GUSTAVO ANTONY COSTA DE SOUZA <20221094010028@ifrn.local>
2025-09-22 11:11:48 -0300

    ...

--- matricula/models.py
+++ matricula/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Aluno(models.Model):
+    nome_aluno = models.CharField(max_length=150)
+    endereco = models.CharField(max_length=250)
+    email = models.EmailField()
+    nasc = models.DateField()
+    sexo = models.CharField(max_length=250)
+    cpf = models.CharField(max_length=11)
+    telefone = models.CharField(max_length=15)
+    cep = models.CharField(max_length=8)
+    bairro = models.CharField(max_length=150)
+    cidade = models.CharField(max_length=250)
+    uf = models.CharField(max_length=2)
+    matricula = models.CharField(max_length=150)
+    curso = models.CharField(max_length=250)
+    serie = models.IntegerField()
+    turno = models.CharField(max_length=150)
+    observacoes = models.TextField()
